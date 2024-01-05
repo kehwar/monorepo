aab106055727f6fc09926f4c80d7f928f4087813
Kehwar <erickkwr@gmail.com>
2024-01-04 23:54:27 -0500

    feat: add indent

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,14 +1,26 @@
 import antfu from '@antfu/eslint-config'
 import type { FlatConfigItem, StylisticConfig } from '@antfu/eslint-config'
 
-const stylisticConfig: StylisticConfig = { indent: 2, quotes: 'single' }
+const stylisticConfig: StylisticConfig = { indent: 4, quotes: 'single' }
 
-export default function (...config: FlatConfigItem[]) {
-  return antfu(
-    {
-      stylistic: stylisticConfig,
-    },
-    // Custom
-    ...config,
-  )
+export function eslintConfig(...config: FlatConfigItem[]) {
+    return antfu(
+        {
+            stylistic: stylisticConfig,
+        },
+        yamlConfig(),
+        // Custom
+        ...config,
+    )
 }
+
+function yamlConfig() {
+    return {
+        files: ['**/*.y?(a)ml'],
+        rules: {
+            'yaml/indent': ['error', 2],
+        },
+    }
+}
+
+export default eslintConfig
