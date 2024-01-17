cfe657ad9bc0c499309e94fbaad19d51b44a1535
= <=>
2024-01-17 16:34:05 -0500

    feat: add vueuse

--- apps/pop-web/nuxt.config.ts
+++ apps/pop-web/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['nuxt-primevue', '@nuxt/image', 'nuxt-lodash', '@nuxt/ui'],
+    modules: ['nuxt-primevue', '@nuxt/image', 'nuxt-lodash', '@nuxt/ui', '@vueuse/nuxt'],
     css: ['primevue/resources/themes/lara-light-purple/theme.css'],
     primevue: {
         components: {
